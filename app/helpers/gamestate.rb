aadcd39dfc5ad65c8e5928e088c997fe236bea7c
Eric Dykstra <eric@ericdykstra.me>
2013-04-19 11:43:15 -0700

    added incomplete board

--- app/helpers/gamestate.rb
+++ app/helpers/gamestate.rb
@@ -13,7 +13,8 @@ module GameState
     res = winner?(a[2],a[5],a[8]);return res if not res == false
     res = winner?(a[0],a[4],a[8]);return res if not res == false
     res = winner?(a[2],a[4],a[6]);return res if not res == false
-    false
+    return true if a.include?('.')
+    return false
   end
 
 end
